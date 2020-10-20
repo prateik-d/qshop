dd381959c351fa8178f0d6f1b133065ef639d732
prateik-d <52733493+prateik-d@users.noreply.github.com>
2020-10-20 17:45:27 +0530

    fetched category data

--- src/app/fashion/fashion.page.ts
+++ src/app/fashion/fashion.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { HamepageService } from 'src/app/shared/hamepage.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-fashion',
@@ -7,6 +10,37 @@ import { Component } from '@angular/core';
 })
 export class fashionPage {
 
-  constructor() {}
+  home_data: any;
+  sub_categories: any;
+
+  id:number=5;
+
+  serverUrl = environment.baseUrl;
+
+  private subscription: Subscription;
+
+
+  constructor(
+    private homepageService: HamepageService
+  ) 
+  {
+  }
+
+  ngOnInit() 
+  {
+      this.subscription = this.homepageService.get_subcat_data(this.id).subscribe((data) => {
+          
+            this.home_data = data.result;
+
+            this.sub_categories = this.home_data.subcategories
+            
+          console.log(this.home_data);
+          console.log(this.sub_categories);
+          
+      });
+  }
+  ngOnDestroy() {
+      this.subscription.unsubscribe();
+  }
 
 }
