5ce9c2fc2b9c70bf598941d4034b047299cf6957
prateik-d <52733493+prateik-d@users.noreply.github.com>
2020-10-20 12:17:48 +0530

    fetched data on subcat page

--- src/app/browse/browse.page.ts
+++ src/app/browse/browse.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { HamepageService } from 'src/app/shared/hamepage.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-browse',
@@ -7,6 +10,35 @@ import { Component } from '@angular/core';
 })
 export class browsePage {
 
-  constructor() {}
+  home_data: any;
+  parent_categories: any;
 
+  serverUrl = environment.baseUrl;
+
+  private subscription: Subscription;
+
+
+  constructor(
+    private homepageService: HamepageService
+  ) 
+  {
+  }
+
+  ngOnInit() 
+  {
+  
+      this.subscription = this.homepageService.get_data().subscribe((data) => {
+          
+            this.home_data = data.result;
+
+            this.parent_categories = this.home_data.parent_categories
+            
+          console.log(this.home_data);
+          console.log(this.parent_categories);
+          
+      });
+  }
+  ngOnDestroy() {
+      this.subscription.unsubscribe();
+  }
 }
